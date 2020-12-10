41de309576327d8f9b8608a41fb12ece239c2c91
Marnix Decloedt <Marnix.Decloedt@realdolmen.com>
2020-12-10 10:06:13 +0100

    fix error

--- frontend/RMotownFestival/src/environments/environment.prod.ts
+++ frontend/RMotownFestival/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   festivalName: 'RMotownFestival',
-  apiBaseUrl: 'https://rmotownfestivalapimd.azurewebsites.net/api',
+  apiBaseUrl: 'https://rmotownfestivalapimd.azurewebsites.net/api/',
 };
